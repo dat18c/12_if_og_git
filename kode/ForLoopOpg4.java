7b6e3170134fb03ff65636faaf2a1452d3ef6b4e
JarlTuxen <jart@kea.dk>
2018-09-14 09:15:27 +0200

    for opg 4

---
+++ kode/ForLoopOpg4.java
@@ -0,0 +1,25 @@
+public class ForLoopOpg4
+{
+   public static void main(String[] args)
+   {
+      //kald printmetode
+      print(3);
+   
+   }
+   
+   public static void print(int antal)
+   {
+      //for loop med 10 gentagelser
+      //for (int i=0;;)
+      //{
+         //udskriv en linie med antal stjerner
+         for (int j=0;j<antal;j++)
+         {
+            //udskriv stjerne og mellemrum
+            System.out.print("* ");
+         }
+         //husk at skifte linie
+         System.out.println();
+      //}
+   }
+}
