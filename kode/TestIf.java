f7ba8f13c3ddac845b373a3306c29756daef230e
JarlTuxen <jart@kea.dk>
2018-09-14 10:32:17 +0200

    TestIf skabelon

--- kode/TestIf.java
+++ kode/TestIf.java
@@ -1,7 +1,9 @@
 public class TestIf
 {
-   //if
-   //if else
-   //if else if else
-   
+   public static void main(String[] args)
+   {
+      //if
+      //if else
+      //if else if else
+   }
 }
