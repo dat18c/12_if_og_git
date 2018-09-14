59ac73a8f4a0e691107f8f4f777e7ad9b39b8f6d
JarlTuxen <jart@kea.dk>
2018-09-14 10:02:54 +0200

    Dog

---
+++ kode/Dog.java
@@ -0,0 +1,28 @@
+public class Dog
+{
+   String navn;
+   String foedselsdato;
+   
+   //konstruktør
+   public Dog()
+   {
+      navn = "";
+      foedselsdato = "1. april 2018";
+   }
+   
+   public Dog(String navn, String dato)
+   {
+      this.navn = navn;
+      this.foedselsdato = dato;
+   }
+   
+   public void logre(boolean glad)
+   {
+   
+   }
+   
+   public void goe(int styrke)
+   {
+   
+   }
+}
